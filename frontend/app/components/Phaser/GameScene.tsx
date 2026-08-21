80998b5c82fe734cfa593302154b6f702a592426
Marllon Wendel <m.w.p.ferreira@outlook.com>
2026-08-21 19:38:55 -0300

    refactor(phaser): use extracted modules in GameScene and PreloadScene

--- frontend/app/components/Phaser/GameScene.tsx
+++ frontend/app/components/Phaser/GameScene.tsx
@@ -1,12 +1,15 @@
 import Phaser from 'phaser';
 import { createPlayer } from '../animations/Phaser/player/player';
 import { configControls, createControls } from '../animations/Phaser/player/controls';
+import { createLamb } from '../animations/Phaser/animes/lamb';
+import { createBuildings } from '../animations/Phaser/buildings/buildings';
 
 export default class GameScene extends Phaser.Scene {
   private player!: Phaser.Physics.Arcade.Sprite;
   private controls!: Phaser.Types.Input.Keyboard.CursorKeys;
   private water!: Phaser.Tilemaps.TilemapLayer;
   private grass!: Phaser.Tilemaps.TilemapLayer;
+  private houses!: Phaser.Physics.Arcade.StaticGroup;
 
   constructor() {
     super('GameScene');
@@ -23,7 +26,7 @@ export default class GameScene extends Phaser.Scene {
     
     const grassLayer = map.createLayer('Grama', tilesExt!, 0, 0);
     const waterLayer = map.createLayer('agua', tileWater!, 0, 0);
-    const roadLayer = map.createLayer('estradas', tileRoad!, 0, 0);
+    map.createLayer('estradas', tileRoad!, 0, 0);
 
     if(!waterLayer) throw new Error('Water layer not found');
 
@@ -32,59 +35,20 @@ export default class GameScene extends Phaser.Scene {
     this.grass = grassLayer as Phaser.Tilemaps.TilemapLayer;
     this.water.setCollisionByProperty({ collider: true });
     this.grass.setCollisionByProperty({ collider: true });
-        this.player = createPlayer(this, 150, 360, 'right');
+  
+    this.player = createPlayer(this, 150, 360, 'right');
 
     this.physics.add.collider(this.player, this.water);
     this.physics.add.collider(this.player, this.grass);
 
-    const firstGid = 1969;
-    const buildingsLayer = map.getObjectLayer('buildings');
-    if (buildingsLayer) {
-      buildingsLayer.objects.forEach((obj) => {
-        const tileId = obj.gid - firstGid;
-        const key = `building_${tileId}`;
-        const sprite = this.physics.add.sprite(obj.x, obj.y, key);
-        sprite.setOrigin(0, 1);
-        sprite.setImmovable(true);
-        
-        // Aplicar escala baseada no tamanho definido no Tiled
-        const scaleX = obj.width / sprite.width;
-        const scaleY = obj.height / sprite.height;
-        sprite.setScale(scaleX, scaleY);
-      });
-    }
+    this.houses = createBuildings(this, map);
 
-    //cabras
-    this.anims.create({
-      key: 'lamb-walk-left',
-      frames: this.anims.generateFrameNumbers('lamb', { start: 12, end: 17 }),
-      frameRate: 8,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: 'lamb-walk-right',
-      frames: this.anims.generateFrameNumbers('lamb', { start: 18, end: 23 }),
-      frameRate: 8,
-      repeat: -1,
-    });
-
-    // Sprite no ponto de inÃ­cio
-    const lamb = this.add.sprite(60.67, 198, 'lamb');
-    lamb.play('lamb-walk-right');
-
-    // Patrulha em loop: direita â esquerda â direita â ...
-    this.tweens.chain({
-      targets: lamb,
-      loop: -1,
-      tweens: [
-        { x: 375.33, duration: 4000, onStart: () => lamb.play('lamb-walk-right') },
-        { x: 60.67,  duration: 4000, onStart: () => lamb.play('lamb-walk-left') },
-      ],
-    });
+    createLamb(this, 60.67, 198);
 
 
-    this.physics.collide(this.player, this.water)
-    this.controls = createControls(this)
+    this.physics.add.collider(this.player, this.houses);
+    this.physics.collide(this.player, this.water);
+    this.controls = createControls(this);
   }
 
   update() {
